ff75a2b6925a3e8ff509faee1a55ae17868f3116
youssef Alaa <youssefpoofon46@gmail.com>
2025-11-24 16:40:52 +0200

    Add source files

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,42 @@
-import Home from './components/Home/Home';
-import Middlepage from './components/Middlepage/Middlepage';
-import Pagethree from './components/Pagethree/Pagethree';
-import Pagefour from './components/Pagefour/Pagefour';
-import Pagefive from './components/Pagefive/Pagefive';
-import Pagesex from './components/Pagesex/Pagesex';
-import Footer from './components/Footer/Footer';
-import Allcars from './components/Allcars/Allcars';
-import Details from './components/Details/Details';
-import { BrowserRouter, RouterProvider, Routes, Route } from 'react-router-dom';
+import Home from "./components/Home/Home";
+import Middlepage from "./components/Middlepage/Middlepage";
+import Pagethree from "./components/Pagethree/Pagethree";
+import Pagefour from "./components/Pagefour/Pagefour";
+import Pagefive from "./components/Pagefive/Pagefive";
+import Pagesex from "./components/Pagesex/Pagesex";
+import Footer from "./components/Footer/Footer";
+import Allcars from "./components/Allcars/Allcars";
+import Details from "./components/Details/Details";
+import SignIn from "./components/SignIn/SignIn";
+import SignUp from "./components/SignUp/SignUp";
+import { BrowserRouter, RouterProvider, Routes, Route } from "react-router-dom";
 function App() {
-
   return (
     <>
       <BrowserRouter>
         <Routes>
-        <Route path="/" element={
-            <>
-              <Home />
-              <Middlepage />
-              <Pagethree />
-              <Pagefour />
-              <Pagefive />
-              <Pagesex />
-              <Footer />
-            </>
-          }
-        />
-        <Route path="/allcars" element={<Allcars />} />
-        <Route path="/detiles" element={< Details/>} />
-        
-      </Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                <Home />
+                <Middlepage />
+                <Pagethree />
+                <Pagefour />
+                <Pagefive />
+                <Pagesex />
+                <Footer />
+              </>
+            }
+          />
+          <Route path="/allcars" element={<Allcars />} />
+          <Route path="/detiles" element={<Details />} />
+          <Route path="/sign_in" element={<SignIn />} />
+          <Route path="/sign_up" element={<SignUp />} />
+        </Routes>
       </BrowserRouter>
     </>
   );
 }
 
-export default App
+export default App;
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styles from "./Home.module.css";
-
+import { Link } from "react-router-dom";
 import logo from "../../assets/imges/Frame (1).png";
 import car from "../../assets/imges/car.png";
 import shape from "../../assets/imges/Frame.png";
@@ -8,46 +8,54 @@ import shape from "../../assets/imges/Frame.png";
 export default function Home() {
   return (
     <div className={styles.header}>
-
       <img className={styles.img2} src={shape} alt="" />
-
       <nav className={styles.navbar}>
         <div className={styles.left}>
           <img src={logo} alt="" />
           <span>RENTCARS</span>
         </div>
-
         <ul className={styles.center}>
-          <li><a href="#">Become a renter</a></li>
-          <li><a href="#">Rental deals</a></li>
-          <li><a href="#">How it work</a></li>
-          <li><a href="#">Why choose us</a></li>
+          <li>
+            <a href="#">Become a renter</a>
+          </li>
+          <li>
+            <a href="#">Rental deals</a>
+          </li>
+          <li>
+            <a href="#">How it work</a>
+          </li>
+          <li>
+            <a href="#">Why choose us</a>
+          </li>
         </ul>
-
         <div className={styles.right}>
-          <a className={styles.signin} href="#">Sign in</a>
-          <a className={styles.signup} href="#">Sign up</a>
+          <Link to={"/sign_in"} className={styles.link}>
+            <a className={styles.signin} href="#">
+              Sign in
+            </a>
+          </Link>
+          <Link to={"/sign_up"} className={styles.link}>
+            <a className={styles.signup} href="#">
+              Sign up
+            </a>
+          </Link>
         </div>
       </nav>
-
       <section className={styles.main}>
-        
         <div>
           <h1 className={styles.textmain}>
             Find, book and <br />
             rent a car <span>Easily</span>
           </h1>
           <p className={styles.text}>
-            Get a car wherever and whenever you need it with your IOS and Android device.
+            Get a car wherever and whenever you need it with your IOS and
+            Android device.
           </p>
         </div>
-
         <div className={styles.car}>
           <img src={car} alt="" />
         </div>
-
       </section>
-
     </div>
   );
 }
